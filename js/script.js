4125f16aeda15916027e4295e5129d2db34de58c
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-10-20 22:08:28 +0200

    fix bonus

--- js/script.js
+++ js/script.js
@@ -41,8 +41,9 @@ header.classList.add("container","text-center","text-dark","biege","p-3","mt-5",
 //.container css e contenuti
 let container=document.getElementById('container');
 container.classList.add("d-flex","container","flex-wrap","biege","text-dark","mt-1" ,"py-4","rounded-4");
-
-
+addPeople();
+function addPeople(){
+    container.innerHTML='';
 for(let element of team){
     let card =document.createElement('div');
     card.classList.add("text-center","col-4","h-50" ,"text-dark","rounded-3","p-2");
@@ -54,24 +55,26 @@ for(let element of team){
         card.innerHTML=
         `
         
-        <img class="rounded-4" class="h-50" src="./img/${element.image}" alt="${element.image}">
+        <img class="rounded-4 img-fluid" class="h-50" src="./img/${element.image}" alt="${element.image}">
         <h3>${element.nome}</h3>
         <p>${element.ruolo}</p>
         
         `
     }
-}
+}}
 //pushare i valori di un form dentro al array team per creare un nuovo elemento del team
 function creaPersona(){
     let nomeAgg=document.getElementById("nomeCognome").value
     let occupazione=document.getElementById("lavoro").value
+    console.log(nomeAgg , occupazione);
     let elemento={
-        nome:"`${nomeAgg}`",
-        ruolo: "`${occupazione}`",
-    
-        image: "sconosciuto.jpeg"
+        nome:nomeAgg ,
+        ruolo: occupazione,
+         image: "sconosciuto.jpeg"
  }
  team.push(elemento);
+ console.log(team)
+ addPeople()
 }
 let btn=document.getElementById('avvia');
 btn.addEventListener('click',creaPersona);
